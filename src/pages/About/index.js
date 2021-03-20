58fc73a218b217eaeb3f3a7804fbd8e9f8f86285
Edmar Costa <edmar.acjr@gmail.com>
2021-03-20 09:16:05 -0300

    atualização da pagina "sobre"

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -8,7 +8,15 @@ export default function About() {
             <Content>
                 <img className="round" src={meuAvatar} alt="foto perfil edmar" />
                 <strong>Edmar Costa</strong>
-                <span>Software Developer at <a target="_blank" rel="noopener noreferrer " href="https://smn.com.br/">SMNTI</a></span>
+                <span>Software Developer at 
+                    <a 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        href="https://mazzafc.tech/"
+                    >
+                        MazzaFC
+                    </a>
+                </span>
                 <span>Franca, São Paulo, Brasil</span>
             </Content>
         </div>
